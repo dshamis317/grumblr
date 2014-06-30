14ad8093b947742f38071cd03b7cf0cf52553738
dshamis317 <dshamis317@gmail.com>
2014-06-30 18:40:07 -0400

    new user creation works

--- app.rb
+++ app.rb
@@ -11,17 +11,17 @@ get '/' do
   erb :index
 end
 
-get 'users/new' do
+get '/users/new' do
   erb :'users/new'
 end
 
-post 'users' do
+post '/users' do
   username = params[:username]
   new_user = User.create({username: username})
   redirect "/users/#{new_user.id}"
 end
 
-get 'users/:id' do
+get '/users/:id' do
   @user = User.find(params[:id])
   erb :'users/show'
 end
